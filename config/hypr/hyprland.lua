54b416b37d1d598ea6d226ebbb810f74c63f6f61
System Wave <systemwave@outlook.com>
2026-07-28 08:14:58 -0500

    adding lua files to conform to main hyprland build

---
+++ config/hypr/hyprland.lua
@@ -0,0 +1,18 @@
+-- Hyprland Configuration ---
+-- Source files located at ~.config/hypr
+
+require("monitors")
+require("bindings")
+require("autostart")
+require("env")
+require("permissions")
+require("looknfeel")
+require("misc")
+require("input")
+require("windowrules")
+
+
+
+
+
+
---
+++ config/hypr/programs.lua
@@ -0,0 +1,13 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+local terminal    = "kitty"
+local fileManager = "thunar"
+local menu        = "wofi"
+
+return {
+    terminal = terminal,
+    fileManager = fileManager,
+    menu = menu,
+}
